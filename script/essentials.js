6bf85c146da4a879a3b7aea357f63c9b7220a008
izuko7 <izuko7mha@gmail.com>
2026-04-22 09:01:47 +0200

    ajout de script permettant de masquer ou d'afficher les mots de passe

--- script/essentials.js
+++ script/essentials.js
@@ -1,3 +1,4 @@
+// Afficher le mot de passe des pages de création de compte
 const input = document.getElementById('password-inscription');
 const incon = document.querySelector('.toggle-password');
 
@@ -8,5 +9,20 @@ function togglePassword() {
     } else {
         input.type = "password";
         incon.classList.replace('fa-eye', 'fa-eye-slash');
+    }
+}
+
+// Afficher le mot de passe de la page de connexion
+
+const input1 = document.getElementById('password');
+const icon1 = document.querySelector('.toggle-password');
+
+function togglePasswordConnexion(){
+    if(input1.type === 'password') {
+        input1.type = 'text';
+        incon1.classList.replace('fa-eye-slash', 'fa-eye');
+    } else {
+        input1.type = "password";
+         incon1.classList.replace('fa-eye', 'fa-eye-slash');
     }
 }
